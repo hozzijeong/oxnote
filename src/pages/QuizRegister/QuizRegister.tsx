047870f3ba96506b5e285d0afc4ff27441775bc2
hozzijeong <ghwls0905@naver.com>
2023-12-05 22:12:53 +0900

    feat: 퀴즈 입력 폼 구현 (아직 생성 로직은 만들어야 함)

---
+++ src/pages/QuizRegister/QuizRegister.tsx
@@ -0,0 +1,120 @@
+import { Input, InputLabel } from '@components/@common';
+import styles from './quizRegister.module.scss';
+import Button from '@components/@common/button';
+import Radio from '@components/@common/radio';
+import { useNavigate } from 'react-router-dom';
+import { URL_PATH } from '@constants/path';
+import { useState } from 'react';
+import { Quiz } from '@models/quiz';
+import { INITIAL_QUIZ } from '@constants/quiz';
+
+const QuizRegister = () => {
+	const navigate = useNavigate();
+
+	const cancelHandler: React.MouseEventHandler<HTMLButtonElement> = () => {
+		const result = confirm(
+			'여기서 취소하면 작성한 내용이 사라집니다. 취소하시겠습니까?'
+		);
+
+		if (result) {
+			navigate(URL_PATH.HOME);
+		}
+	};
+
+	const [quizState, setQuizState] = useState<Quiz>(INITIAL_QUIZ);
+
+	const inputHandler = <T extends HTMLInputElement | HTMLTextAreaElement>(
+		event: React.ChangeEvent<T>
+	) => {
+		if (!(event instanceof HTMLInputElement || HTMLTextAreaElement)) return;
+
+		const { name, value, type } = event.target;
+
+		setQuizState((prev) => ({
+			...prev,
+			[name]: type === 'radio' ? Boolean(Number(value)) : value,
+		}));
+	};
+
+	const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<main className={styles.main} onSubmit={submitHandler}>
+			<form className={styles['quiz-form']}>
+				<InputLabel
+					title='문제'
+					element={
+						<Input
+							id='quiz'
+							mode='text'
+							name='quiz'
+							value={quizState.quiz}
+							onChange={inputHandler}
+							required
+						/>
+					}
+					htmlFor='quiz'
+				/>
+
+				<InputLabel
+					title='답'
+					htmlFor='answer'
+					element={
+						<Radio
+							options={[
+								{ title: 'O', value: 1 },
+								{ title: 'X', value: 0 },
+							]}
+							name='answer'
+							changeHandler={inputHandler}
+							required
+						/>
+					}
+				/>
+
+				<InputLabel
+					title='해설'
+					element={
+						<textarea
+							className={styles.explain}
+							value={quizState.explain}
+							onChange={inputHandler}
+							name='explain'
+							required
+						/>
+					}
+					htmlFor='explain'
+				/>
+
+				<InputLabel
+					title='즐겨찾기 등록'
+					htmlFor='favorite'
+					element={
+						<Radio
+							options={[
+								{ title: '등록하기', value: 1 },
+								{ title: '등록안함', value: 0 },
+							]}
+							name='favorite'
+							changeHandler={inputHandler}
+							required
+						/>
+					}
+				/>
+
+				<div className={styles['button-container']}>
+					<Button type='button' size='small' onClick={cancelHandler}>
+						취소하기
+					</Button>
+					<Button type='submit' size='small' color='primary'>
+						등록하기
+					</Button>
+				</div>
+			</form>
+		</main>
+	);
+};
+
+export default QuizRegister;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export { default as RootTemplate } from './RootTemplate/RootTemplate';
+export { default as QuizRegister } from './QuizRegister/QuizRegister';
