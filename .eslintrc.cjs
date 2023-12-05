d09cc222e5d58255955d3f9947acdb1a0c05bca2
hozzijeong <ghwls0905@naver.com>
2023-12-05 13:41:03 +0900

    chore: 각종 패키지 설정 및 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,109 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs', vite.config.ts],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'react/button-has-type': 1,
+		'react/function-component-definition': [
+			2,
+			{ namedComponents: 'arrow-function' },
+		],
+		'react/react-in-jsx-scope': 0,
+		'react/jsx-pascal-case': 1,
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					'type',
+					'builtin',
+					'external',
+					'internal',
+					['parent', 'sibling', 'index'],
+					'unknown',
+				],
+				pathGroups: [
+					{
+						pattern: '/type$',
+						group: 'type',
+						position: 'before',
+					},
+					{
+						pattern: 'react*',
+						group: 'builtin',
+						position: 'before',
+					},
+					{
+						pattern: '@pages/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@components/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '\\./(.*)\\.style$',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@hooks/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@contexts/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@apis/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@utils/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@constants/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '@style/*',
+						group: 'internal',
+						position: 'after',
+					},
+					{
+						pattern: '^\\./?',
+						group: 'internal',
+						position: 'after',
+					},
+				],
+				alphabetize: {
+					order: 'asc',
+				},
+			},
+		],
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+		'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+	},
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sass from 'sass';
+import tsconfigPath from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tsconfigPath()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				implementation: sass,
+			},
+		},
+	},
+	resolve: {
+		alias: [
+			{ find: '@apis', replacement: resolve(__dirname, '/src/apis') },
+			{ find: '@models', replacement: resolve(__dirname, '/src/models') },
+			{ find: '@pages', replacement: resolve(__dirname, '/src/pages') },
+			{
+				find: '@components',
+				replacement: resolve(__dirname, '/src/components'),
+			},
+			{ find: '@hooks', replacement: resolve(__dirname, '/src/hooks') },
+			{ find: '@utils', replacement: resolve(__dirname, '/src/utils') },
+			{ find: '@assets', replacement: resolve(__dirname, '/src/assets') },
+			{ find: '@constants', replacement: resolve(__dirname, '/src/constants') },
+			{ find: '@style', replacement: resolve(__dirname, '/src/style') },
+			{ find: '@contexts', replacement: resolve(__dirname, '/src/contexts') },
+		],
+	},
+});
