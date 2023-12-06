a5e1528385aeac6704a4c59eceaa5aafc7e3cacc
hozzijeong <ghwls0905@naver.com>
2023-12-06 22:47:18 +0900

    feat: 카테고리 grid 디자인 완료

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -2,19 +2,32 @@ import useGetCategory from '@hooks/fireStore/useGetCategory';
 import styles from './category.module.scss';
 import { useMemo } from 'react';
 import CategoryInput from '@components/category/categoryInput/CategoryInput';
+import { Link, generatePath } from 'react-router-dom';
+import { URL_PATH } from '@constants/path';
+
+import Folder from '@assets/folder.svg';
 
 const Category = () => {
 	const { data: category } = useGetCategory();
 
 	const categories = useMemo(
-		() => category.map((name) => <div>{name}</div>),
+		() =>
+			category.map((name) => (
+				<Link
+					className={styles['category-folder']}
+					to={generatePath(URL_PATH.CATEGORY_DETAIL, { id: name })}
+				>
+					<img src={Folder} width={72} height={72} alt={`${name} 폴더`} />
+					{name}
+				</Link>
+			)),
 		[category]
 	);
 
 	return (
 		<main className={styles.main}>
 			<CategoryInput category={category} />
-			<section>{categories}</section>
+			<section className={styles['category-container']}>{categories}</section>
 		</main>
 	);
 };
