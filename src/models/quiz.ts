8143c1178b7fd56cd0086b6db7e20ece15485e60
hozzijeong <ghwls0905@naver.com>
2023-12-07 19:17:33 +0900

    refactor: quizListItem 타입 추가

--- src/models/quiz.ts
+++ src/models/quiz.ts
@@ -1,9 +1,10 @@
+// TODO QUIZ 문제 발생하는지 확인하기
 export interface Quiz {
 	quiz: string;
 	explain: string;
 	answer: boolean;
 	favorite: boolean;
-	category: Category;
+	category: Pick<Category, 'name'>; // 여기의 카테고리는 좀 애매한 느낌임
 }
 
 export interface QuizInfo extends Quiz {
@@ -16,4 +17,12 @@ export interface QuizInfo extends Quiz {
 export interface Category {
 	name: string;
 	id: number;
+}
+
+export interface QuizListItem
+	extends Omit<
+		QuizInfo,
+		'category' | 'answer' | 'explain' | 'recentCorrect' | 'answer'
+	> {
+	wrongPercent: `${string}%`;
 }
