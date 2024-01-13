4eadbef0a3481c67c68ef6df84c0a1fb8e8b0548
hozzijeong <ghwls0905@naver.com>
2024-01-13 21:50:15 +0900

    refactor: Quiz 페이지 내부에서 이동하는 함수 로직 분리

---
+++ src/hooks/useRedirectQuiz.ts
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import useLocationQueryParams from './useLocationQueryParams';
+import { useCallback, useTransition } from 'react';
+import { QUIZ_PARAMS } from '@constants/quiz';
+import { URL_PATH } from '@constants/path';
+
+const useRedirectQuiz = () => {
+	const navigate = useNavigate();
+
+	const { setQueryParam, getCurrentParams } = useLocationQueryParams();
+
+	const [_, startTransition] = useTransition();
+
+	const redirectQuizHandler = useCallback((path: string) => {
+		setQueryParam(QUIZ_PARAMS.quiz, path);
+		startTransition(() => {
+			navigate(`${URL_PATH.QUIZ}?${getCurrentParams()}`, {
+				replace: true,
+			});
+		});
+	}, []);
+
+	return redirectQuizHandler;
+};
+
+export default useRedirectQuiz;
