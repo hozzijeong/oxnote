5b25f3818d834d121fad3aeb39603b0dc63bb1ff
hozzijeong <ghwls0905@naver.com>
2024-02-15 18:22:36 +0900

    refactor: 인증되지 않은 사용자라면 login 페이지로 redirect 하도록 설정

--- src/pages/RootTemplate/RootTemplate.tsx
+++ src/pages/RootTemplate/RootTemplate.tsx
@@ -1,26 +1,39 @@
 import { Navbar } from '@components/@common';
 import { NAVBAR_PAGE, URL_PATH } from '@constants/path';
-import { Suspense, useMemo } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Suspense, useEffect, useMemo, useState } from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import styles from './rootTemplate.module.scss';
 import Spinner from '@components/@common/spinner';
-import { auth } from '@fireStore/Firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@fireStore/Firebase';
+import useCurrentUser from '@hooks/auth/useCurrentUser';
 
 const RootTemplate = () => {
 	const location = useLocation();
-	const navigate = useNavigate();
+	const { user, setUser } = useCurrentUser();
 
 	const navbar = useMemo(
 		() => (NAVBAR_PAGE.includes(location.pathname) ? <Navbar /> : null),
 		[location.pathname]
 	);
 
-	onAuthStateChanged(auth, (user) => {
-		if (!user) {
-			navigate(URL_PATH.LOGIN, { replace: true });
-		}
-	});
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			setUser(user);
+
+			setLoading(false);
+		});
+	}, []);
+
+	if (loading) {
+		return <Spinner />;
+	}
+
+	if (!loading && user === null) {
+		return <Navigate to={URL_PATH.LOGIN} />;
+	}
 
 	return (
 		<div className={styles.container}>
