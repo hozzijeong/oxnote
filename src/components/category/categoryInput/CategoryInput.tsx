34b86c8bb71a66e8836450d9b7a7bbf36b68fd67
hozzijeong <ghwls0905@naver.com>
2024-02-29 22:03:46 +0900

    design: input 사이즈 수정

--- src/components/category/categoryInput/CategoryInput.tsx
+++ src/components/category/categoryInput/CategoryInput.tsx
@@ -20,7 +20,7 @@ const CategoryInput = ({ category }: CategoryInputProps) => {
 				onChange={inputChangeHandler}
 			/>
 			<Button
-				size='small'
+				size='large'
 				className={styles['add-button']}
 				type='submit'
 				onClick={addCategoryHandler}
